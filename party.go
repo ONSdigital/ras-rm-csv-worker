0a38405449e4dc71c1730f950b703b097e675f65
Warren Bailey <warren@warrenbailey.net>
2021-05-24 11:33:52 +0100

    use int rather than string

--- party.go
+++ party.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"fmt"
 	"errors"
+	"strconv"
 )
 
 type Party struct {
@@ -22,34 +23,34 @@ type Party struct {
 }
 
 type Attributes struct {
-	CHECKLETTER   string `json:"checkletter,omitempty"`
-	FROSIC92      string `json:"frosic92,omitempty"`
-	RUSIC92       string `json:"rusic92,omitempty"`
-	FROSIC2007    string `json:"frosic2007,omitempty"`
-	RUSIC2007     string `json:"rusic2007,omitempty"`
-	FROEMPMENT    string `json:"froempment,omitempty"`
-	FROTOVER      string `json:"frotover,omitempty"`
-	ENTREF        string `json:"entref,omitempty"`
-	LEGALSTATUS   string `json:"legalstatus,omitempty"`
-	NAME		  string `json:"name,omitempty"`
-	ENTREPMKR     string `json:"entrepmkr,omitempty"`
-	REGION        string `json:"region,omitempty"`
-	BIRTHDATE     string `json:"birthdate,omitempty"`
-	ENTNAME1      string `json:"entname1,omitempty"`
-	ENTNAME2      string `json:"entname2,omitempty"`
-	ENTNAME3      string `json:"entname3,omitempty"`
-	RUNAME1       string `json:"runame1,omitempty"`
-	RUNAME2       string `json:"runame2,omitempty"`
-	RUNAME3       string `json:"runame3,omitempty"`
-	TRADSTYLE1    string `json:"tradstyle1,omitempty"`
-	TRADSTYLE2    string `json:"tradstyle2,omitempty"`
-	TRADSTYLE3    string `json:"tradstyle3,omitempty"`
-	SELTYPE       string `json:"seltype,omitempty"`
-	INCLEXCL      string `json:"inclexcl,omitempty"`
-	CELLNO        string `json:"cellNo,omitempty"`
-	FORMTYPE      string `json:"formType,omitempty"`
-	CURRENCY      string `json:"currency,omitempty"`
-	SAMPLEUNITID  string `json:"sampleUnitRef,omitempty"`
+	CHECKLETTER   string `json:"checkletter"`
+	FROSIC92      string `json:"frosic92"`
+	RUSIC92       string `json:"rusic92"`
+	FROSIC2007    string `json:"frosic2007"`
+	RUSIC2007     string `json:"rusic2007"`
+	FROEMPMENT    int `json:"froempment"`
+	FROTOVER      int `json:"frotover"`
+	ENTREF        string `json:"entref"`
+	LEGALSTATUS   string `json:"legalstatus"`
+	NAME		  string `json:"name"`
+	ENTREPMKR     string `json:"entrepmkr"`
+	REGION        string `json:"region"`
+	BIRTHDATE     string `json:"birthdate"`
+	ENTNAME1      string `json:"entname1"`
+	ENTNAME2      string `json:"entname2"`
+	ENTNAME3      string `json:"entname3"`
+	RUNAME1       string `json:"runame1"`
+	RUNAME2       string `json:"runame2"`
+	RUNAME3       string `json:"runame3"`
+	TRADSTYLE1    string `json:"tradstyle1"`
+	TRADSTYLE2    string `json:"tradstyle2"`
+	TRADSTYLE3    string `json:"tradstyle3"`
+	SELTYPE       string `json:"seltype"`
+	INCLEXCL      string `json:"inclexcl"`
+	CELLNO        int `json:"cellNo"`
+	FORMTYPE      string `json:"formType"`
+	CURRENCY      string `json:"currency"`
+	SAMPLEUNITID  string `json:"sampleUnitRef"`
 
 }
 
@@ -77,8 +78,8 @@ func newParty(line []byte, sampleSummaryId string, sampleUnitId string) *Party {
 		RUSIC92:       setIfNotEmpty(&sample[3]),
 		FROSIC2007:    setIfNotEmpty(&sample[4]),
 		RUSIC2007:     setIfNotEmpty(&sample[5]),
-		FROEMPMENT:    setIfNotEmpty(&sample[6]),
-		FROTOVER:      setIfNotEmpty(&sample[7]),
+		FROEMPMENT:    convertToInt(sample[6]),
+		FROTOVER:      convertToInt(sample[7]),
 		ENTREF:        setIfNotEmpty(&sample[8]),
 		LEGALSTATUS:   setIfNotEmpty(&sample[9]),
 		NAME:		   "",
@@ -96,7 +97,7 @@ func newParty(line []byte, sampleSummaryId string, sampleUnitId string) *Party {
 		TRADSTYLE3:    setIfNotEmpty(&sample[21]),
 		SELTYPE:       setIfNotEmpty(&sample[22]),
 		INCLEXCL:      setIfNotEmpty(&sample[23]),
-		CELLNO:        setIfNotEmpty(&sample[24]),
+		CELLNO:        convertToInt(sample[24]),
 		FORMTYPE:      setIfNotEmpty(&sample[25]),
 		CURRENCY:      setIfNotEmpty(&sample[26]),
 		SAMPLEUNITID:  sampleUnitId,
@@ -117,6 +118,15 @@ func setIfNotEmpty(value *string) string {
 	} else {
 		return *value
 	}
+}
+
+func convertToInt(value string) int {
+	v, err := strconv.Atoi(value)
+	if err != nil {
+		logger.Error("error converting to int",  zap.Error(err))
+		v = 0
+	}
+	return v
 }
 
 func (p *Party) sendToPartyService() error {
