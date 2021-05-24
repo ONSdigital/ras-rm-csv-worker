f03a7b8db037298f26bf0972b0ff8d46f69a5ae9
Warren Bailey <warren@warrenbailey.net>
2021-05-24 10:18:15 +0100

    reference by memory

--- party.go
+++ party.go
@@ -17,8 +17,7 @@ type Party struct {
 	SAMPLEUNITREF string `json:"sampleUnitRef"`
 	SAMPLESUMMARYID string `json:"sampleSummaryId"`
 	SAMPLEUNITTYPE string `json:"sampleUnitType""`
-	attributes Attributes `json:attributes`
-
+	attributes *Attributes `json:attributes`
 	msg             *pubsub.Message `json:"-"`
 }
 
@@ -106,8 +105,7 @@ func newParty(line []byte, sampleSummaryId string, sampleUnitId string) *Party {
 		SAMPLEUNITREF: sample[0],
 		SAMPLESUMMARYID: sampleSummaryId,
 		SAMPLEUNITTYPE: "B",
-		attributes: *attr,
-
+		attributes: attr,
 	}
 	logger.Debug("party created", zap.String("SAMPLEUNITREF", party.SAMPLEUNITREF))
 	return party
